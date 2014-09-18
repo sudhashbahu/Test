adafff3ce951b67d5f05af790b916190e9e4ee72
sudhashbahu <sudhashbahu@yahoo.com>
2014-09-18 18:44:32 +0530

    Updated

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,38 @@
-﻿var app = {
+﻿function changeText(elem, changeVal) {
+     if ((elem.textContent) && (typeof (elem.textContent) != "undefined")) {
+          elem.textContent = changeVal;
+     } else {
+          elem.innerText = changeVal;
+     }
+}
+
+function onSuccess(position) {
+     var elem = document.getElementById('Latitude');
+     changeText(elem, position.coords.latitude);
+     elem = document.getElementById('Longitude');
+     changeText(elem, position.coords.longitude);
+     elem = document.getElementById('Altitude');
+     changeText(elem, position.coords.altitude);
+     elem = document.getElementById('Accuracy');
+     changeText(elem, position.coords.accuracy);
+     elem = document.getElementById('AltitudeAccuracy');
+     changeText(elem, position.coords.altitudeAccuracy);
+     elem = document.getElementById('Heading');
+     changeText(elem, position.coords.heading);
+     elem = document.getElementById('Speed');
+     changeText(elem, position.coords.speed);
+     elem = document.getElementById('Timestamp');
+     changeText(elem, position.timestamp);
+}
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+     alert('code: ' + error.code + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+var app = {
      // Application Constructor
      initialize: function() {
           this.bindEvents();
@@ -20,6 +54,8 @@
      // Update DOM on a Received Event
      receivedEvent: function(id) {
           var Element = document.getElementById('AppText');
-          Element.innerHTML = "Hello";
+          changeText(Element, 'Started');
+
+          navigator.geolocation.getCurrentPosition(onSuccess, onError, { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true });
      }
 };
